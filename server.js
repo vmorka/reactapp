c374dd9bbab55048e099ea30d15f2e7b209f1787
lukas druskis <lukas.druskis@panko.lt>
2018-10-31 14:20:11 +0200

    paejo

--- server.js
+++ server.js
@@ -2,10 +2,14 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
+const songs = require('./routes/api/songs');
+
 const app = express();
 
 // Bodyparser Middleware
 app.use(bodyParser.json());
+  
+
 
 // DB config
 const db = require('./config/keys').mongoURI;
@@ -14,6 +18,10 @@ const db = require('./config/keys').mongoURI;
 mongoose.connect(db, { useNewUrlParser: true })
     .then(() => console.log('MongoDB Prijungta...'))
     .catch(err => console.log(err));
+
+//use routes
+
+app.use('/api/songs', songs);
 
 const port = process.env.PORT || 5000;
 
