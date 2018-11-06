59ca3a8b477c179d7751b6365de20b0608abe787
Vytautas Morkūnas <vytautas.morkunas@panko.lt>
2018-11-06 16:31:09 +0200

    test

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -8,6 +8,7 @@ class Login extends Component {
           <input type="text" placeholder="Įveskite el. paštą"/>
           <input type="password" placeholder="Įveskite slaptažodį"/>
           <button type="submit">Prisijungti</button>
+          <button type="button">bimbam</button>
         </form>
       </div>
     );
